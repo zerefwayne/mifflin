b606bc928436876e7cdcf490308f599337dfcc40
Aayush Joglekar <aayushjog@gmail.com>
2020-04-13 23:08:11 +0530

    Removed service command

--- utils/cmd.util.go
+++ utils/cmd.util.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os/exec"
 	"strings"
 )
@@ -43,5 +45,43 @@ func CommandRunner(cmd string) {
 	} else {
 		fmt.Println(string(resp[:]))
 	}
+
+}
+
+func FetchSubServices(service string) []string {
+
+	cmdString := fmt.Sprintf("systemctl list-unit-files | grep %s", service)
+
+	cmd := exec.Command("bash", "-c", cmdString)
+
+	var subServices []string
+
+	if stdout, err := cmd.StdoutPipe(); err != nil {
+		log.Fatal(err)
+	} else {
+
+		if err := cmd.Start(); err != nil {
+			log.Fatal(err)
+		} else {
+
+			defer cmd.Wait()
+
+			buff := bufio.NewScanner(stdout)
+
+			var returnText []string
+
+			for buff.Scan() {
+				returnText = append(returnText, buff.Text())
+			}
+
+			for _, subService := range returnText {
+				subServices = append(subServices, strings.Fields(subService)[0])
+			}
+
+		}
+
+	}
+
+	return subServices
 
 }
